36c89614a6fa2499baaa227f20572ce80935abb0
emmaabraham04 <156918206+emmaabraham04@users.noreply.github.com>
2026-02-03 09:54:35 -0500

    updating code to fix image display

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -7,7 +7,7 @@ function About() {
       <nav className="navbar background">
         <ul className="nav-list">
           <div className="logo">
-            <img src="hugo.jpeg" alt="hugo"/>
+            <img src="/hugo.jpeg" alt="hugo" />
           </div>
           <li>
             <a href="#about">About</a>
