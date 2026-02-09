dbb06f310e9160d63d261b1f249e3e4bcb2afb1a
DreadGod96 <theelimonroe@gmail.com>
2026-02-09 10:41:29 -0500

    updated dev branch

---
+++ features/about_service/src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+
--- shared/lib/db.js
+++ shared/lib/db.js
@@ -7,23 +7,37 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //Create db connection
+<<<<<<< HEAD
 export const db_connection_pool = mysql.createPool({
+=======
+export const db_connection_pool = mysql.createConnection({
+>>>>>>> 6d27f18 (updated dev branch)
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWD,
     database: process.env.DB_NAME,
+<<<<<<< HEAD
     port: DB_PORT,
     ssl: {
         ca: fs.readFileSync(path.resolve(__dirname, '../certs/global-bundle.pem')),
+=======
+    port: 3306,
+    ssl: {
+        ca: fs.readFileSync(path.resolve(__dirname, '../certs/global-budle.pem')),
+>>>>>>> 6d27f18 (updated dev branch)
         rejectUnauthorized: true
     }, 
     waitForConnections: true,
     connectionLimit: 10,
+<<<<<<< HEAD
     maxIdle: 10,
     idleTimeout: 60000,
     queueLimit: 0,
     enableKeepAlive: true
     keepAliveInitialDelay: 0
+=======
+    queueLimit: 0,
+>>>>>>> 6d27f18 (updated dev branch)
 });
 
 //Test connection on startup
