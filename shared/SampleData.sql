995af971793f420eea6c4c1ec817c4cd6bb36407
Nolen Schnabel <nschnab@clemson.edu>
2026-02-09 16:02:21 -0500

    Create Sample Data

---
+++ shared/SampleData.sql
@@ -0,0 +1,25 @@
+call add_user( 
+'nschnab',
+'Nolen',
+'Schnabel',
+'admin',
+current_date(),
+'000-000-0000',
+'nschnab@clemson.edu',
+1,
+@user_ID
+);
+
+select * from User;
+
+insert into Company (
+    company_name,
+    company_contract_start,
+    company_region,
+    company_phone
+) values (
+	'Clemson University',
+    current_date(),
+    'US-East',
+    '864-656-0000'
+);
