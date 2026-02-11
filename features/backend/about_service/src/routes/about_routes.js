3e4160b75de923c57becc63f7a86426cd673aa27
DreadGod96 <theelimonroe@gmail.com>
2026-02-11 16:21:11 -0500

    updated var names and fixed issue w/ promises. Postman verified successful local GET from about_service table

--- features/backend/about_service/src/routes/about_routes.js
+++ features/backend/about_service/src/routes/about_routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { get_data_row } from '../controllers/about_controller.js';
+import { pull_data } from '../controllers/about_controller.js';
 
 const router = express.Router();
-router.get('/', get_data_row);
+router.get('/', pull_data);
 export default router;
