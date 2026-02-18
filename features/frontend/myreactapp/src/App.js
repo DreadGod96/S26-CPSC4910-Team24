786311b6785114730457890c23a73a181907be8e
DreadGod96 <theelimonroe@gmail.com>
2026-02-18 07:22:03 -0500

    Updated amplify YAML and reverted to original format for testing

--- features/frontend/myreactapp/src/App.js
+++ features/frontend/myreactapp/src/App.js
@@ -46,7 +46,7 @@ function About() {
   const [posts, setPosts] = React.useState([]);
 
   useEffect(() => {
-    const url = import.meta.env.ABOUT_API_URL;
+    const url = process.env.REACT_APP_ABOUT_URL || 'http://localhost:3001/api/about';
     const fetchAboutData = async () => {
       try {
         const response = await fetch(url);
