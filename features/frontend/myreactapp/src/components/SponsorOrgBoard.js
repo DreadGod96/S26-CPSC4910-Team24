cf5d2128c8a6c393a5b155c9157328e8c0ab5409
stetterton <127871788+stetterton@users.noreply.github.com>
2026-02-25 14:07:36 -0500

    Added typescript for sponsor org dashboard

---
+++ features/frontend/myreactapp/src/components/SponsorOrgBoard.js
@@ -0,0 +1,24 @@
+import "./DashBoard.css";
+export default function Dashboard() {
+  return (
+    <div className="dashboard">
+      <div className="welcome">Welcome back, Sponsor</div>
+
+      <div className="card small">
+        <p>Manage sponor users</p>
+      </div>
+
+      <div className="card large">TBD</div>
+
+      <div className="card small">
+        <p>
+          Last catalog report
+          <br />
+          12/20/25
+        </p>
+      </div>
+
+      <div className="card large">Account Settings</div>
+    </div>
+  );
+}
