5932319f24e5ed73cf6f8943cddd94ca8b72b407
stetterton <127871788+stetterton@users.noreply.github.com>
2026-02-26 14:52:05 -0500

    Sponsor board now routs to sponsor user

--- features/frontend/myreactapp/src/components/SponsorOrgBoard.js
+++ features/frontend/myreactapp/src/components/SponsorOrgBoard.js
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import "./DashBoard.css";
+
 export default function Dashboard() {
+  const navigate = useNavigate();
+
   return (
     <div className="dashboard">
       <div className="welcome">Welcome back, Sponsor</div>
 
-      <div className="card small">
-        <p>Manage sponor users</p>
+      {/* Onclick handling */}
+      <div
+        className="card small"
+        onClick={() => navigate("/sponsboard")}
+        style={{ cursor: "pointer" }}
+      >
+        <p>Manage sponsor users</p>
       </div>
 
       <div className="card large">View Order history</div>
