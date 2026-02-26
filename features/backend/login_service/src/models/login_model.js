5921f97bf70aa710fc9f3add1dcb205f9704317b
Elijahday Monroe <emonro2@clemson.edu>
2026-02-26 17:12:37 -0500

    added login_model

--- features/backend/login_service/src/models/login_model.js
+++ features/backend/login_service/src/models/login_model.js
@@ -4,7 +4,7 @@ export const findUser = async (user_email) => {
     try {
         const sql_cmd = `
         SELECT u.user_ID,  u.user_email, u.user_role, l.password_hash
-        FROM user u  JOIN login l ON u.user_ID = l.user_ID
+        FROM user u  JOIN Login l ON u.user_ID = l.user_ID
         WHERE u.user_email = ?`;
         
         const [rows] = await getPool().query(sql_cmd, [user_email]);
