9bcbb6feb45f8e85e4cdd8cce30f3c27f5c8c987
emmaabraham04 <156918206+emmaabraham04@users.noreply.github.com>
2026-03-02 10:29:32 -0500

    Update server.js

--- features/backend/points_service/src/server.js
+++ features/backend/points_service/src/server.js
@@ -11,7 +11,7 @@ const env_path = '../../../../environs/development/.env';
 dotenv.config({ path: path.resolve(__dirname, env_path) });
 
 const app = express();
-const PORT = process.env.POINTS_PORT || 3003;
+const PORT = process.env.POINTS_PORT || 3004;
 
 const config = {
     origin: [
