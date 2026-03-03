8ba7ddf95ab43b237c845414f53f374363395072
Nolen Schnabel <nschnab@clemson.edu>
2026-03-03 16:11:29 -0500

    Revert "Merge pull request #9 from DreadGod96/dominos"
    This reverts commit ad39c4b810a3b38bc8d17df87bdb195d8652abc6, reversing
    changes made to 15f52c6e74aad877ddabbf82d020a26af27081c5.

--- features/backend/dominosAPI/src/routes/dominos_route.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { getDominosMenu } from '../controllers/dominos_controller.js';
-
-const router = express.Router();
-router.post('/get_menu', getDominosMenu);
-export default router;
