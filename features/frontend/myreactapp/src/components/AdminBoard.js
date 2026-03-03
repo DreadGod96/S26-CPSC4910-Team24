463f349b6fad6ea19387d5ac7866f8cc5b1be422
stetterton <127871788+stetterton@users.noreply.github.com>
2026-03-03 15:52:34 -0500

    Fixed lack of constant variable for navigation

--- features/frontend/myreactapp/src/components/AdminBoard.js
+++ features/frontend/myreactapp/src/components/AdminBoard.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "./DashBoard.css";
 export default function Dashboard() {
+  const navigate = useNavigate();
   return (
     <div className="dashboard">
       <div className="welcome">Welcome back, Admin</div>
