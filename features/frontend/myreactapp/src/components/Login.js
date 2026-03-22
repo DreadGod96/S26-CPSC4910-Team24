bb0c44f42ab63c77813a678d5c87abd896668f32
EC2 Default User <ec2-user@ip-172-31-76-115.ec2.internal>
2026-03-22 22:08:03 +0000

    fixed env call

--- features/frontend/myreactapp/src/components/Login.js
+++ features/frontend/myreactapp/src/components/Login.js
@@ -40,7 +40,7 @@ export default function Login() {
     }
 
     try {
-      const response = await fetch(REACT_APP_LOGIN_URL,
+      const response = await fetch(process.env.REACT_APP_LOGIN_URL,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
