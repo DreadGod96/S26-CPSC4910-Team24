d4eab57c18dedbebd1cdb057e68474d6776ce19a
emmaabraham04 <156918206+emmaabraham04@users.noreply.github.com>
2026-03-31 08:29:56 -0400

    Update application_routes.js

--- features/backend/application_service/src/routes/application_routes.js
+++ features/backend/application_service/src/routes/application_routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { post_data, get_company_list, get_company_id_by_name } from '../controllers/application_controller.js';
+import { post_data, get_company_list, get_company_id_by_name, get_driver_applications } from '../controllers/application_controller.js';
 
 const router = express.Router();
 router.post('/', post_data);
 router.get('/companylist', get_company_list);
 router.get('/getcompanyid', get_company_id_by_name);
+router.get('/driver/:driver_id', get_driver_applications);
 export default router;
