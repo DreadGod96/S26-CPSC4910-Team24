05fd9d22ac0ad480a1b8ba29cec758a903011113
emonro2 <emonro2@clemson.edu>
2026-04-05 14:56:09 -0400

    reorder routes

--- features/backend/user_service/src/routes/user_routes.js
+++ features/backend/user_service/src/routes/user_routes.js
@@ -11,10 +11,10 @@ import {
 const router = express.Router();
 
 router.get('/', pull_users);
+router.put('/settings/:id', update_settings_controller);
+router.put('/change-password/:id', change_password_controller);
 router.get('/:id', pull_user_by_id);
 router.put('/:id', update_user_field_controller);
-router.put('/settings/:id', update_settings_controller);
 router.delete('/:id', delete_user_controller);
-router.put('/change-password/:id', change_password_controller);
 
 export default router;
